f6cfa4204826e2c2c9d0dd6d61b85c3f28d27dd5
EspiFreelancer <espifreelancer@gmail.com>
2020-12-25 20:15:48 -0300

    Add Real-time DB listener. Update username and channel

--- scripts/chat.js
+++ scripts/chat.js
@@ -3,6 +3,7 @@ class Chatroom {
 		this.channel = channel;
 		this.username = username;
 		this.chats = db.collection('chats');
+		this.unsubscribe;
 	}
 
 	// create documents structure
@@ -19,11 +20,55 @@ class Chatroom {
 		const response = await this.chats.add(chat);
 		return response;
 	}
+
+	// real-time listener to monitoring collection "chats" in db
+	getChats(callbackFunc) {
+		this.unsubscribe = this.chats
+			.where('channel', '==', this.channel)
+			.orderBy('created_at')
+			.onSnapshot(snapshot => {
+				snapshot.docChanges().forEach(change => {
+					if (change.type === 'added') {
+						// update UI
+						callbackFunc(change.doc.data());
+					}
+				})
+			})
+	}
+
+	// update username
+	updateUsername(username) {
+		this.username = username;
+	}
+
+	// update channel, unsubscribe from changes.
+	updateChannel(channel) {
+		this.channel = channel;
+		console.log('channel updated');
+		if (this.unsubscribe) {
+			this.unsubscribe();
+		}
+	}
 }
 
-// const chatroom = new Chatroom('Música', 'José');
+const chatroom = new Chatroom('Música', 'José');
 // console.log(chatroom);
 
 // chatroom.addChat('Los Enanitos Verdes me gustan mucho')
 // 	.then(() => console.log('mensaje de chat agregado'))
 // 	.catch(err => console.log(err));
+
+chatroom.getChats(data => {
+	console.log(data);
+});
+
+
+// Channel change test
+// setTimeout(() => {
+// 	chatroom.updateChannel('General');
+// 	chatroom.updateUsername('Marcelo');
+// 	chatroom.getChats(data => {
+// 		console.log(data);
+// 	});
+// 	chatroom.addChat('Hola a todos');
+// }, 3000);
